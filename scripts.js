72f683bd5dd5fcfcd27d002b7f247e6e12d7376d
Carl Jnr <youngjnr@outlook.com>
2024-03-24 14:19:26 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,3 +9,16 @@ const isStudent= true;
 
 console.log(names);
 console.log(isStudent);
+prompt("welcome to my site")
+
+//Arithmetic operators
+let sum = 10-5;
+let difference =10-5;
+let product = 10*5;
+let quotient = 10/5;
+
+prompt (sum)
+prompt (difference)
+prompt (product)
+prompt (quotient)
+
